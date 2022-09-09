a7eb59395d0be9245725cf34a2c955f6284da996
Leonard Norblad <norblad88@gmail.com>
2022-09-09 14:41:11 +0200

    safe

--- JLPack/R/dijkstra.R
+++ JLPack/R/dijkstra.R
@@ -1,10 +1,30 @@
+#'Dijkstra
+#'
+#'Finds the shortest distance to each vertex in the graph from the initial node.
+#'
+#'
+#'@param graph A data.frame with three numeric variables named \code{v1}, \code{v2} and \code{w}. \code{v1} and \code{v2} are the graph edges, and  \code{w} is the corresponding weight of the edges.
+#'@param init_node A numeric scalar that exist in the graph.
+#' 
+#' 
+#'@return A vector containing distances of the shortest path between the initial node and each vertices in order.
+#'@examples
+#'
+#'graph <- data.frame(v1 = c(1,1,1,2,2,2,2,3,3,3,4,4,5,5,6,6),
+#'v2 = c(2,3,4,1,3,4,6,1,2,5,1,2,3,6,2,5),
+#'w = c(5,3,10,5,7,2,14,3,7,1,10,2,1,7,14,7))
+#'
+#'@references
+#'Wikipedia contributors. (2022, August 21). Dijkstra's algorithm. In Wikipedia, The Free Encyclopedia. Retrieved 12:39, September 9, 2022, from \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#'
+
 dijkstra <- function(graph, init_node){
   
   # checks the input
   if (init_node <= 0 |         # init_node has to be greater than 0
       !is.numeric(init_node)|  # init_node has to be a numeric value
       !is.data.frame(graph) |  # graph needs to be a data.frame
-      any(!is.numeric(c(graph$v1, graph$v2, graph$w))) |  # "v1", "v2" and "w" has to be numeric
+     any(!is.numeric(c(graph$v1, graph$v2, graph$w))) |  # "v1", "v2" and "w" has to be numeric
       ! any(colnames(graph) %in% c("v1", "v2", "w"))      # graph needs to be named: "v1", "v2" and "w"
   ) { stop()}
   
@@ -35,5 +55,3 @@ dijkstra <- function(graph, init_node){
   }
   return(dist)                        # returns a vector of the shortest distances (stored during the tests)
 }
-
-
