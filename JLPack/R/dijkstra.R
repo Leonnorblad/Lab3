6f339ee6b44d4b492e823bfef2c8b621afd3e297
Leonard Norblad <norblad88@gmail.com>
2022-09-09 15:05:27 +0200

    safe

--- JLPack/R/dijkstra.R
+++ JLPack/R/dijkstra.R
@@ -3,7 +3,7 @@
 #'Finds the shortest distance to each vertex in the graph from the initial node.
 #'
 #'
-#'@param graph A data.frame with three numeric variables named \code{v1}, \code{v2} and \code{w}. \code{v1} and \code{v2} are the graph edges, and  \code{w} is the corresponding weight of the edges.
+#'@param graph A \code{data.frame} with three numeric variables named \code{v1}, \code{v2} and \code{w}. \code{v1} and \code{v2} are the graph edges, and  \code{w} is the corresponding weight of the edges.
 #'@param init_node A numeric scalar that exist in the graph.
 #' 
 #' 
@@ -14,8 +14,9 @@
 #'v2 = c(2,3,4,1,3,4,6,1,2,5,1,2,3,6,2,5),
 #'w = c(5,3,10,5,7,2,14,3,7,1,10,2,1,7,14,7))
 #'
+#'dijkstra(graph, 1)
 #'@references
-#'Wikipedia contributors. (2022, August 21). Dijkstra's algorithm. In Wikipedia, The Free Encyclopedia. Retrieved 12:39, September 9, 2022, from \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#'Wikipedia contributors. (2022, August 21). Dijkstra's algorithm. In Wikipedia, The Free Encyclopedia. Retrieved 12:39, September 9, 2022, from \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm} 
 #'
 
 dijkstra <- function(graph, init_node){
