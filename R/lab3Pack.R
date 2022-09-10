36809ebfeefb561085cbdfd160c1565efcf62959
Leonard Norblad <norblad88@gmail.com>
2022-09-10 10:47:03 +0200

    s

--- R/lab3Pack.R
+++ R/lab3Pack.R
@@ -1,7 +1,10 @@
-#' Lab 3 ...
+#' Lab 3!
 #'
+#' Does good things
 #'
-#'
-#'
-#'
-#'
+#' @docType package
+#'  
+#' @author Leon \email{leope744@student.liu.se}
+#' @author Jaycob \email{JacXXX@student.liu.se}
+#' @name JLPack
+NULL
